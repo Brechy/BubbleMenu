995b2812fa8c90ebac888cd9527186a2cce65c46
brechin <brechin@brechy.net>
2019-05-13 13:16:24 -0700

    add initial animation to bubble

--- App.js
+++ App.js
@@ -5,12 +5,29 @@ import {
   View,
   Dimensions 
 } from 'react-native';
+import { Easing } from 'react-native-reanimated';
 
 //responsive
 const {width, height} = Dimensions.get('window');
 const size = Math.min(width, height) - 1;
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      scale: new Animated.Value(0)
+    };
+  }
+  
+  componentDidMount() {
+    Animated.timing(this.state.scale, {
+      toValue: 4,
+      duration: 1000,
+      easing: Easing.linear
+    }).start();
+  };
+  
   render() {
     return (
       <View style={styles.container}>
@@ -19,7 +36,10 @@ export default class App extends React.Component {
           backgroundColor: '#ccc',
           width: size,
           height: size,
-          borderRadius: size / 2
+          borderRadius: size / 2,
+          transform: [{
+            scale: this.state.scale
+          }]
         }} />
       </View>
     );
