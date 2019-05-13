cbd7284ff2b9ae273200aa15428e6996cc703fc7
brechin <brechin@brechy.net>
2019-05-13 13:36:11 -0700

    add animation of bubble on interaction

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import {
   StyleSheet,
   Animated,
   View,
-  Dimensions 
+  Dimensions,
+  TouchableWithoutFeedback 
 } from 'react-native';
 import { Easing } from 'react-native-reanimated';
 
@@ -27,21 +28,56 @@ export default class App extends React.Component {
       easing: Easing.linear
     }).start();
   };
+
+  getLeftPosition () {
+    const halfSize = size / 2;
+    const halfWidth = width / 2;
+    
+    let marginHorizontalTopLeft = -halfSize;
+
+    return marginHorizontalTopLeft + halfWidth;
+  }
+
+  getTopPosition () {
+    const halfSize = size / 2;
+
+    let marginVerticalTopLeft = -halfSize;
+
+    return marginVerticalTopLeft + height;
+  }
+
+  onPress() {
+    Animated.timing(this.state.scale, {
+      toValue: 4,
+      duration: 1000,
+      easing: Easing.linear
+    }).start(() => {
+      this.setState({
+        scale: new Animated.Value(0)
+      });
+    });
+  }
   
   render() {
+    let topPosition = this.getTopPosition();
+    let leftPosition = this.getLeftPosition();
     return (
-      <View style={styles.container}>
-        <Animated.View style={{
-          position: 'absolute',
-          backgroundColor: '#ccc',
-          width: size,
-          height: size,
-          borderRadius: size / 2,
-          transform: [{
-            scale: this.state.scale
-          }]
-        }} />
-      </View>
+      <TouchableWithoutFeedback onPress={this.onPress.bind(this)}>
+        <View style={styles.container}>
+          <Animated.View style={{
+            position: 'absolute',
+            backgroundColor: '#ccc',
+            width: size,
+            height: size,
+            borderRadius: size / 2,
+            left: leftPosition,
+            top: topPosition,
+            transform: [{
+              scale: this.state.scale
+            }]
+          }} />
+        </View>
+      </TouchableWithoutFeedback> 
     );
   }
 }
